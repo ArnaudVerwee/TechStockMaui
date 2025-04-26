78ad46abe457793b717dcfd7e10382ba179b0dcf
ArnaudVerwee <122737752+ArnaudVerwee@users.noreply.github.com>
2025-04-26 16:26:17 +0200

    Add project files.

---
+++ TechStockMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TechStockMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TechStockMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TechStockMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
